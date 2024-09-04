5c18c9c841e5b35e784cac5ec1efdbad7f99e829
Maulidya Isnaini <163633669+mauldynan@users.noreply.github.com>
2024-09-04 07:04:57 +0700

    coba aja

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div className='max-w-screen-lg mx-auto flex flex-col justify-center items-center text-center w-full'>
-      <p className='text-base font-serif mb-5'>Hello👋 My name is Maulidya Isnaini, and I'm</p>
+      <p className='text-base font-serif mb-5'>Hellooooo👋 My name is Maulidya Isnaini, and I'm</p>
       <p className=' font-bold font-serif text-7xl text-yellow-300 mb-5'>GERDA SOWULO</p>
       <p className='text-base font-serif mb-2'>As Front End Developer</p>
       <a href="#" className="text-base font-serif text-white bg-black bg-opacity-50 py-3 px-12 rounded-full 
